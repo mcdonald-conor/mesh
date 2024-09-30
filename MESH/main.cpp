9756746f33bbf592a9f3f2bb06c1ec2f5c9e51b5
Conor McDonald <cm@mcdonaldconor.com>
2024-09-30 10:34:52 +0100

    Initial Commit

---
+++ MESH/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MESH
+//
+//  Created by Conor McDonald on 30/09/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
